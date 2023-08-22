ed1fc2f8f46ec3694636ed330236a00cc6b20263
Darkynsky <telles861@gmail.com>
2023-08-21 22:07:26 -0300

    Aula02

---
+++ ReactNative/IntroducaoAoJS/Aula02.js
@@ -0,0 +1,16 @@
+const obj = {
+    nome: "Vinicius",
+    signo: "Libra",
+    filme: ["Resident Evil", "Anjos da Noite"]
+}
+// Formato JSON - JavaScript Notation Object
+ console.log(obj.nome, obj.signo)
+ console.log(obj)
+ console.log(typeof obj)
+ console.log(obj.filme[0])
+
+ let pessoa = ["Vinicius", 27, "Desenvolvedor"]
+ console.log(pessoa[0], pessoa[2])
+ console.log(pessoa)
+ console.log(typeof pessoa)
+
