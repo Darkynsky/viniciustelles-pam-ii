e6fccce634cd5c94628ccb7f7eb07b947991cc82
Darkynsky <telles861@gmail.com>
2023-11-13 22:10:02 -0300

    13-11

---
+++ AndroidStudio/06-11/cadastro/app/src/main/java/com/example/cadastro/Alunos.java
@@ -0,0 +1,109 @@
+package com.example.cadastro;
+
+public class Alunos {
+    private String rmaluno;
+    private String nome;
+    private String email;
+    private String celular;
+    private String telefone;
+    private String endereco;
+    private String complemento;
+    private String estado;
+    private String cidade;
+    private String cep;
+
+
+
+    public Alunos (){
+    }
+
+    public String getRmaluno() {
+        return rmaluno;
+    }
+
+    public void setRmaluno(String rmaluno) {
+        this.rmaluno = rmaluno;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    private String curso;
+}
--- AndroidStudio/06-11/cadastro/app/src/main/java/com/example/cadastro/MainActivity.java
+++ AndroidStudio/06-11/cadastro/app/src/main/java/com/example/cadastro/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference minhaReferencia = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        DatabaseReference alunos = minhaReferencia.child("alunos");
+        Alunos a = new Alunos();
+
+
     }
 }
